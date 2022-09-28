68a5fb63406d04b943c23da368eaceb70cbb7d50
shariyer <s.m.shariyer04@gmail.com>
2022-09-28 23:09:31 +0600

    activity card added

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -4,7 +4,11 @@ import React from 'react';
 const Activity = () => {
     return (
         <div className='single-activity'>
-            
+            <img src="" alt="" />
+            <h3>Activity Name:</h3>
+            <p>Benefit:</p>
+            <h6>Age:</h6>
+            <h6>Time:</h6>
         </div>
     );
 };
